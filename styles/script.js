0a848911a3a08881b838d43eefbc4d21a1467691
srubinic <31941197+srubinic@users.noreply.github.com>
2018-08-02 09:20:54 -0700

    Add files via upload

---
+++ styles/script.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("mynav").style.visibility = "visible";
+ }
+
+function closeNav() {
+    document.getElementById("mynav").style.visibility = "hidden";
+}
